bb5b52fef5298362e732329d3363a1fc77bba60d
DOMKA <software.domka@gmail.com>
2026-02-17 18:44:53 -0500

    Update MeetingsList.jsx

--- frontend/src/components/MeetingsList.jsx
+++ frontend/src/components/MeetingsList.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-const API_URL = 'http://localhost:3000';
+//const API_URL = 'http://localhost:3000';
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 function MeetingsList() {
   const [meetings, setMeetings] = useState([]);
